3cf611b85ca88e131148579dbf9c8fb37b290c74
Alex Gutler <alex11jgt@hotmail.com>
2015-05-18 15:52:38 -0300

    implementação do gerenciamento dos assets pelo elixir

--- readme.md
+++ readme.md
@@ -1,12 +1,7 @@
 ## CodeCommerce
 
-## Projeto Fase 9
+## Projeto Fase 10
 
-### Loja
+### Assets
 
-Agora que você já possui a base para gerar a home da Loja, você precisa:
-
-- Listar os produtos em desta
-- Listar os produtos recomendados
-
-Quando clicarmos em um item da categoria, deveremos exibir apenas os produtos de uma determinada categoria.
+Repita exatamente o aprendido nesse capítulo para que você possa gerenciar seus assets utilizando o Elixir.
--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -7,12 +7,9 @@
     <meta name="author" content="">
     <title>@yield('title', 'Home | CodeCommerce')</title>
 
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/prettyPhoto.css')}}" rel="stylesheet">
-    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
-    <link href="{{asset('css/main.css')}}" rel="stylesheet">
-    <link href="{{asset('css/responsive.css')}}'" rel="stylesheet">
+    {{--<link href="{{asset('css/all.css')}}" rel="stylesheet">--}}
+    <link href="{{elixir('css/all.css')}}" rel="stylesheet">
+
 </head><!--/head-->
 
 <body>
@@ -135,11 +132,7 @@
 
 </footer><!--/Footer-->
 
-<script src="{{asset('js/jquery.js')}}"></script>
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
-<script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
-<script src="{{asset('js/jquery.prettyPhoto.js')}}"></script>
-<script src="{{asset('js/main.js')}}"></script>
+<script src="{{elixir('js/all.js')}}"></script>
 
 </body>
 </html>
